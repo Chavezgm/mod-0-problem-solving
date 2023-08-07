4f34099dcbcd3d2d16c8f87a99a32d4ccaceb172
Martin Chavez <martinchavez1203@gmail.com>
2023-08-07 15:20:21 -0600

    add comment explanations

--- challange_1.rb
+++ challange_1.rb
@@ -5,3 +5,7 @@ names = ["Martin","Luis","Lucy","Rodrigo","Jose","Alex"]
 filter_names= names.select{|name|name.length == 4}
 puts filter_names
 
+# We define an array by callin it names 
+# We use .select to filter throughout the array and select all the strings that have 4 characters 
+# we store the filtered names list under filter_names
+# We then use puts filter_name and this prints all the names with 4 chracters
