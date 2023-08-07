fa09f00510049eefe7f47dcb5c2bdb0eb293a865
Martin Chavez <martinchavez1203@gmail.com>
2023-08-07 01:32:53 -0600

    Finish chall 1

--- challange_1.rb
+++ challange_1.rb
@@ -1,3 +1,7 @@
 #1. Given an array of strings, print only the strings that have exactly 4 characters.
 
 
+names = ["Martin","Luis","Lucy","Rodrigo","Jose","Alex"]
+filter_names= names.select{|name|name.length == 4}
+puts filter_names
+
